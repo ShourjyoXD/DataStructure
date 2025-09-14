82353111a013285a6cc7a4137288581ebf5c0f64
ShourjyoXD <shourjyochakraborty2006@gmail.com>
2025-09-14 19:57:44 +0530

    day 3

--- week_1/Day_3.c
+++ week_1/Day_3.c
@@ -1,1 +1,15 @@
 //Input 5 numbers in an array and print them.
+#include <stdio.h>
+
+int main() {
+    int arr[5];
+    for(int i=0; i<5;i++){
+        printf("enter an element: ");
+        scanf("%d", &arr[i]);
+    }
+    for(int i=0; i<5;i++){
+        printf("%d\n", arr[i]);
+        
+    }
+    return 0;
+}
